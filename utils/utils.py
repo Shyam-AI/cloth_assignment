e04d6e9bb865f94ecd405a0ec918d1625ac418d9
Shyam-AI <shyamdl2803@gmail.com>
2022-08-17 17:50:30 +0530

    updated utils file

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,9 @@ import base64
 
 
 def decodeImage(imgstring, fileName):
+    """decoding the inpt image
+        Author: Shyam U
+    """
     imgdata = base64.b64decode(imgstring)
     with open(fileName, 'wb') as f:
         f.write(imgdata)
